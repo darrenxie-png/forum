4a902709170ed5192db65c5262a3a3c18dc376f0
Darren <darren.xie@pkmgindo.com>
2026-07-03 10:38:38 +0700

    fix folder

--- forum-api/src/Domains/users/entities/RegisteredUser.js
+++
@@ -1,9 +0,0 @@
-class RegisteredUser {
-  constructor({ id, username, fullname }) {
-    this.id = id;
-    this.username = username;
-    this.fullname = fullname;
-  }
-}
-
-module.exports = RegisteredUser;
